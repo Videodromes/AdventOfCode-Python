e784f3a8760882f2c29ed04b29909c76af4a4350
Videodromes <19981579+Videodromes@users.noreply.github.com>
2024-11-12 14:26:15 -0500

    Update formatting with Black package

--- 2015/01_not_quite_lisp/1.py
+++ 2015/01_not_quite_lisp/1.py
@@ -1,9 +1,11 @@
-with open('2015/01_not_quite_lisp/input.txt', 'r', encoding="utf-8") as input:
+with open("2015/01_not_quite_lisp/input.txt", "r", encoding="utf-8") as input:
     content = input.read()
+
 
 # Part 1
 def final_floor():
-    return content.count('(') - content.count(')')
+    return content.count("(") - content.count(")")
+
 
 # Part 2
 def first_basement():
@@ -13,12 +15,13 @@ def first_basement():
         if floor == -1:
             return index
         else:
-            if char == '(':
+            if char == "(":
                 floor += 1
                 index += 1
-            elif char == ')':
+            elif char == ")":
                 floor -= 1
                 index += 1
+
 
 print(f"Part 1: {final_floor()}")
 print(f"Part 2: {first_basement()}")
--- 2015/03_perfectly_spherical_houses_in_a_vacuum/3.py
+++ 2015/03_perfectly_spherical_houses_in_a_vacuum/3.py
@@ -1,52 +1,57 @@
-with open('2015/03_perfectly_spherical_houses_in_a_vacuum/input.txt', 'r', encoding='utf-8') as input:
+with open(
+    "2015/03_perfectly_spherical_houses_in_a_vacuum/input.txt", "r", encoding="utf-8"
+) as input:
     data = input.read()
 
 x_pos, y_pos = 0, 0
 
+
 def part1(x, y):
     grid = {(x, y)}
-    
+
     for direction in data:
-        if direction == '^':
+        if direction == "^":
             y += 1
-        elif direction == 'v':
+        elif direction == "v":
             y -= 1
-        elif direction == '>':
+        elif direction == ">":
             x += 1
-        elif direction == '<':
+        elif direction == "<":
             x -= 1
-        
+
         grid.add((x, y))
-    
+
     return len(grid)
+
 
 def part2(x, y):
     robo_x, robo_y = x, y
     grid_santa = {(x, y)}
-    grid_robo = {(x,y)}
-    
+    grid_robo = {(x, y)}
+
     for i, direction in enumerate(data):
         if i % 2 == 0:
-            if direction == '^':
+            if direction == "^":
                 y += 1
-            elif direction == 'v':
+            elif direction == "v":
                 y -= 1
-            elif direction == '>':
+            elif direction == ">":
                 x += 1
-            elif direction == '<':
+            elif direction == "<":
                 x -= 1
             grid_santa.add((x, y))
         else:
-            if direction == '^':
+            if direction == "^":
                 robo_y += 1
-            elif direction == 'v':
+            elif direction == "v":
                 robo_y -= 1
-            elif direction == '>':
+            elif direction == ">":
                 robo_x += 1
-            elif direction == '<':
+            elif direction == "<":
                 robo_x -= 1
-            grid_robo.add((robo_x,robo_y))
-    
+            grid_robo.add((robo_x, robo_y))
+
     return len(grid_santa.union(grid_robo))
+
 
 print(f"Part 1: {part1(x_pos, y_pos)}\nPart 2: {part2(x_pos, y_pos)}")
